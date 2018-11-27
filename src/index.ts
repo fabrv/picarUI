6157f844d89bac80965382de820a593372ee3f0b
Fabrizzio Rivera <frivera@nevicad.com>
2018-11-27 12:37:54 -0600

    init yml

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from './App'
 import chalk from 'chalk';
+import fs from 'fs'
 
 const port: any = process.env.PORT || 3000
 
@@ -11,6 +12,14 @@ app.server.listen(port, '0.0.0.0', (err: any) => {
 
   app.io.on('connect', (socket: SocketIO.Socket) => {
     socket.on('run-state', (data)=>{
+      let i = data ? 1 : 0;
+      fs.writeFile('../init.yml',`s: ${i}`,()=>{
+        // throws an error, you could also catch it here
+        if (err) throw err;
+
+        // success case, the file was saved
+        console.log('INIT YML guardado');
+      })
       app.on = data
       console.log(chalk.red(`Iniciando/apagando piCAR-S, ${data}`))
     })
